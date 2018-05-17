5bde1d28fb174e417a2a900dff0ec5cddd62eb22
nemusou <ryubi.102395@gmail.com>
2018-05-17 23:12:41 +0900

    lsとかの関数の記述は別でやったほうがいい気がする

--- source/Dish.d
+++ source/Dish.d
@@ -21,20 +21,17 @@ class Dish {
     this() {
         this.USER_NAME = environment["USER"];
         this.PATH = environment["PATH"].split(":");
-        this.PATH.writeln;
-        this.current_dir.writeln;
-        // typeid(current_dir).writeln;
-        // current_dir.to!(string);
-        // typeid(current_dir).writeln;
 
         commands = [
-            "ls": Command("ls", (string[] args) {
-                foreach (string e; dirEntries(getcwd, SpanMode.shallow)) {
-                    e.split("/")[$ - 1].writeln;
-                }
-                return 1;
-            }),
+            "ls": Command("ls", &this.ls),
         ];
+    }
+
+    int ls(string[] args) {
+        foreach(string e; dirEntries(getcwd, SpanMode.shallow)) {
+            e.split("/")[$ - 1].writeln;
+        }
+        return 1;
     }
 }
 
