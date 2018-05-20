3e6a899f727f78e1c3d00940b205d54856797a61
nemusou <ryubi.102395@gmail.com>
2018-05-21 01:15:44 +0900

    Dish classを使って動くようにした

--- source/app.d
+++ source/app.d
@@ -3,73 +3,39 @@ import std.file;
 import std.process;
 import std.string;
 import std.path : buildPath;
-import core.thread : Thread;
-import core.sys.posix.unistd : fork;
 
-auto dish_funcs = [&dish_ls, &dish_exit];
+import Command : Command;
+import Dish : Dish;
 
-ulong[string] dish_func_names;
-
-void prompt() {
-    const userName = environment["USER"];
-    writef(userName ~ " > ");
-}
-
-int dish_ls(string current_dir) {
-    foreach (string e; dirEntries(current_dir, SpanMode.shallow)) {
-        e.split("/")[$ - 1].writeln;
-    }
-    writeln;
-    return 1;
-}
-
-int dish_exit(string current_dir) {
-    writeln("exit dish. good bye!");
-    return -1;
-}
 
 string read_line() {
     string line = readln;
     if (line[$ - 1] == '\n') {
         return line.chomp;
     }
-    return null;
-}
-
-int exec_command(string command, string current_dir) {
-    auto args = command.split;
-    if (args.length == 0) {
-        return 0;
-    }
-
-    int status;
-    if (command in dish_func_names) {
-        status = dish_funcs[dish_func_names[command]](current_dir);
-    }
-    else {
-        auto pid = spawnShell(command);
-        status = pid.wait;
-    }
-
-    return status;
+    return line;
 }
 
 void d_shell() {
-    dish_func_names["ls"] = dish_func_names.length;
-    dish_func_names["exit"] = dish_func_names.length;
-    const USER_NAME = environment["USER"];
-    string current_dir = getcwd;
-    auto PATH = environment["PATH"].split(":");
+    auto dish = new Dish;
 
-    writefln("Hello, %s. Let's Hack!", USER_NAME);
+    writefln("Hello, %s. Let's Hack!\n\n", environment["USER"]);
 
-    int status;
+    int status = 1;
     do {
-        prompt;
+        dish.commands["prompt"].command([]);
         string command = read_line;
-        status = exec_command(command, current_dir);
+
+        if (command == "") {
+            continue;
+        }
+
+        Command* p = (command.split[0] in dish.commands);
+        if (p != null) {
+            status = p.command([]);
+        }
     }
-    while (status != -1);
+    while (status > 0);
 }
 
 void main() {
