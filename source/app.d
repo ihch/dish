77e79848622f1c6c68cd7781675befa09efaad22
nemusou <ryubi.102395@gmail.com>
2018-05-30 23:09:11 +0900

    コマンドの呼び出し引数に入力を渡すようにした

--- source/app.d
+++ source/app.d
@@ -32,7 +32,7 @@ void d_shell() {
 
         Command* p = (command.split[0] in dish.commands);
         if (p != null) {
-            status = p.command([]);
+            status = p.command([command]);
         }
         else {
             // " 2> /dev/null" 標準エラー出力を虚無の世界に投げています
