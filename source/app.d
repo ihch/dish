e9b13ccf4ced525ccaba308dd84521c849b4f383
nemusou <ryubi.102395@gmail.com>
2019-10-18 13:36:38 +0900

    feat: 一文字ずつの入力読み取りをできるようにした

--- source/app.d
+++ source/app.d
@@ -1,3 +1,4 @@
+import core.sys.posix.termios;
 import std.stdio;
 import std.file;
 import std.process;
@@ -8,17 +9,50 @@ import Command : Command;
 import Dish : Dish;
 
 
+int getch()
+{
+    int ch;
+    termios oldt;
+    termios newt;
+
+    tcgetattr(0, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(0, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(0, TCSANOW, &oldt);
+
+    ch.write;
+    // write(cast(char) ch);
+    return ch;
+}
+
 /++
   read user input command
 
   return: result string
 +/
 string read_line() {
-    string line = readln;
-    if (line[$ - 1] == '\n') {
-        return line.chomp;
-    }
-    return line;
+    string buffer = "";
+
+    do {
+        int c = getch();
+        if (c == 27) {
+          getch();
+          c = getch();
+          if (c == 65) write("<up>");
+          if (c == 66) write("<down>");
+          if (c == 67) write("<right>");
+          if (c == 68) write("<left>");
+          continue;
+        }
+        if (c == 127) write("<BS>");
+        buffer ~= c;
+        // write(c);
+    } while (buffer.length == 0 || buffer[$ - 1] != '\n');
+    writeln(buffer);
+
+    return buffer.chomp;
 }
 
 /++
